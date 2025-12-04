328e274ed17949221b0f73d0d96ffb919c5fd7bd
Delight <delightgeorge105@gmail.com>
2025-12-04 11:48:40 +0100

    removed updateuser controller from user router

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const userRouter = express.Router();
-const { registerUser, loginUser, updateUserController } = require('../controllers/userController');
+const { registerUser, loginUser } = require('../controllers/userController');
 const Uploads = require('../middlewares/uploads');
 const { isUser } = require('../middlewares/auth');
 
 userRouter.post("/registerUser", Uploads.single("image"), registerUser);
 userRouter.post("/loginUser",  loginUser);
-userRouter.put("/update-user", isUser, updateUserController);
 
 
 module.exports = userRouter;
