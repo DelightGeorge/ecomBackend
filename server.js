1101be0334afe016bf1cb34a15fe5a6e1dda0fbf
Delight <delightgeorge105@gmail.com>
2025-12-04 11:06:18 +0100

    fixed cors

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   cors({
-    origin: ['http://localhost:5173', 'https://granduer-steel.vercel.app/'],
+    origin: ['http://localhost:5173', 'https://granduer-steel.vercel.app'],
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     credentials: true,
   })
