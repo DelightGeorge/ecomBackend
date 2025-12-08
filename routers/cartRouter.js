5f76e2bf0b516066d94f32108630ca5637a26993
Delight <delightgeorge105@gmail.com>
2025-12-08 11:40:03 +0100

    edited cart router

--- routers/cartRouter.js
+++ routers/cartRouter.js
@@ -9,15 +9,105 @@ const { isUser } = require("../middlewares/auth");
 
 const cartRouter = express.Router();
 
+
+/**
+ * @swagger
+ * /addcart:
+ *   post:
+ *     summary: Add a product to the user's cart
+ *     tags:
+ *       - Cart
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               productId:
+ *                 type: string
+ *               quantity:
+ *                 type: integer
+ *             required:
+ *               - productId
+ *               - quantity
+ *     responses:
+ *       200:
+ *         description: Product added to cart successfully
+ */
 // Add product to cart
 cartRouter.post("/addcart", isUser, addToCart);
 
+/**
+ * @swagger
+ * /getcart:
+ *   get:
+ *     summary: Get the current user's cart
+ *     tags:
+ *       - Cart
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: Returns the user's cart
+ */
 // Get user cart
 cartRouter.get("/getcart", isUser, getCart);
-
+/**
+ * @swagger
+ * /updatecart:
+ *   patch:
+ *     summary: Update a product in the user's cart
+ *     tags:
+ *       - Cart
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               productId:
+ *                 type: string
+ *               quantity:
+ *                 type: integer
+ *             required:
+ *               - productId
+ *               - quantity
+ *     responses:
+ *       200:
+ *         description: Product updated in cart successfully
+ */
 // Update product in cart
 cartRouter.patch("/updatecart", isUser, updateCart);
-
+/**
+ * @swagger
+ * /deletecart:
+ *   post:
+ *     summary: Delete a product from the user's cart
+ *     tags:
+ *       - Cart
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               productId:
+ *                 type: string
+ *             required:
+ *               - productId
+ *     responses:
+ *       200:
+ *         description: Product removed from cart successfully
+ */
 // Delete product from cart
 cartRouter.post("/deletecart", isUser, deleteCart); // accept productid in body
 
